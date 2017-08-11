90537bde6330ba3641a835c107e9eff27591b4bf
heb264 <heb264@gmail.com>
2017-08-11 10:54:00 +0800

    added week 4 - 6

---
+++ week06/ch02/js/login.js
@@ -0,0 +1,32 @@
+// login.js
+
+// Function called when the form is submitted.
+// Function validates the form data and returns a Boolean value.
+
+	
+    // Get references to the form elements:
+
+
+
+    // Validate!
+
+
+
+
+ // End of validateForm() function.
+
+// Function called when the window has been loaded.
+// Function needs to add an event listener to the form.
+
+
+    
+    // Confirm that document.getElementById() can be used:
+
+
+
+
+
+// End of init() function.
+
+// Assign an event listener to the window's load event:
+
---
+++ week06/ch04/js/text.js
@@ -0,0 +1,37 @@
+// Script  - text.js
+// This script limits the amount of text that can be entered into a textarea.
+
+// Function called when the form is submitted.
+// Function limits the text and returns false.
+
+
+	
+	// For storing the limited text:
+	
+
+    
+    // Get a reference to the form value:
+	
+
+	// Find the last space before 100 characters:
+	
+	
+	// Trim the text to that spot:
+	
+	
+	// Display the number of characters submitted:
+	
+
+	// Display the limitedText:
+	
+	
+	// Return false to prevent submission:
+	
+    
+ // End of limitText() function.
+
+// Function called when the window has been loaded.
+// Function needs to add an event listener to the form.
+
+ // End of init() function.
+
